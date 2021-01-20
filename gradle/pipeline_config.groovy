dad83b9715c0b4508994545164cc9b8aa8c7d0cc
dtiwari <dtiwari@loyalty.com>
2021-01-20 15:11:08 -0500

    add nodeJS dir

---
+++ gradle/pipeline_config.groovy
@@ -0,0 +1,29 @@
+// restrict individual repository Jenkinsfiles
+allow_scm_jenkinsfile = false
+// skip the default JTE checkout and do it explicitly
+skip_default_checkout = false
+// define application environment objects
+application_environments{
+    dev{
+        long_name = "Development"
+    }
+    prod{
+        long_name = "Production"
+    }
+}
+
+/*
+  define libraries to load.
+  available libraries are based upon the
+  library sources configured.
+*/
+libraries{
+    git{
+        branch = "dev"
+    }
+    gradle
+    docker
+    aws
+    sonarqube
+    unittest
+}
